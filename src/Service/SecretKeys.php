f948d5eb36b5337c257f00fa4bf09ff9ba49723f
Roderick Espina <derick813@yahoo.co.uk>
2018-12-08 23:36:45 +0000

    Update SecretKeys.php
    updated for security reason

--- src/Service/SecretKeys.php
+++ src/Service/SecretKeys.php
@@ -15,8 +15,8 @@ class SecretKeys
      */
     private function keys(){
         return [
-            'api_key_cityBike' => 'eca092226d4b38574f27fbb08dd30c2882eb12f7',
-            'api_key_weather' => '15b073f5f4a04bdf90e4ad74941d0cad',
+            'api_key_cityBike' => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+            'api_key_weather' => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
         ];
     }
 
